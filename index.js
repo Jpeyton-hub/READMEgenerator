3fbced48e40c5313c957f2ddcd4e7cf681ab8eb1
Jpeyton <jpwalk319@gmail.com>
2021-02-16 20:28:54 -0700

    created the basic template literal to fill in for markdown.

--- index.js
+++ index.js
@@ -4,6 +4,53 @@ let template = '';
 // needs title, description, table of contents, installation, usage, license(checkboxes, populate badge and details in license section), contributing, tests and questions(github username into link and email)
 
 
-function templateGen() {
+function templateGen(input) {
+    template = 
+    `># ${input.title}
+    ---
 
+    #### ${input.description}
+    ---
+
+    >## Table of Contents
+    ---
+    - [Installation]()
+    - [Usage]()
+    - [License]()
+    - [Contribution]()
+    - [Tests]()
+    - [Questions]()
+    
+    >### Installation
+    
+    ${input.installation}
+    ---
+    
+    >### Usage
+    
+    ${input.usage}
+    ---
+
+    >### License
+    
+    ${licenseInfo}
+    ---
+
+    >### Contribution
+    
+    ${input.contribution}
+    ---
+
+    >### Tests
+
+    ${input.tests}
+    ---
+    
+    >### Questions
+    
+    If you have a questions please connect with me on Github [${input.username}](https://github.com/${input.username})
+    or send me an email at <${input.email}>
+    ---
+    
+    #### Thanks for Viewing!`
 }
