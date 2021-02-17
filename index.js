d6179c2f2c8c1fb9bea8c19d8a0656028edabc2d
Jpeyton <jpwalk319@gmail.com>
2021-02-16 20:34:19 -0700

    made template to be created by arrow function

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const inquire = require('inquirer');
 const fs = require('fs');
-let template = '';
+
 // needs title, description, table of contents, installation, usage, license(checkboxes, populate badge and details in license section), contributing, tests and questions(github username into link and email)
 
 
-function templateGen(input) {
-    template = 
+const templateGen = (input) => { 
     `># ${input.title}
     ---
 
