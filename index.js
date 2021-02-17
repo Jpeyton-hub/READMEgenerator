af161ff3ee49c8a75433b894543a2eb7e2bd806e
Jpeyton <jpwalk319@gmail.com>
2021-02-17 10:27:51 -0700

    added the prompt function and most of the prompt info

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
 const inquire = require('inquirer');
 const fs = require('fs');
+// prompt from inquirer
+
+const promptUser = () =>
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please type a brief description of your project.',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Input installation instructions you want displayed.',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Input your usage instructions.',
+    },
+    {
+      type:'checkbox',
+      name:'license',
+      message: 'Which license would you like displayed?',
+      choices: []
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Input your contribution instructions',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Input testing information',
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'What is your GitHub username?'  
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?'  
+    }
+  ]);
+
 
 // needs title, description, table of contents, installation, usage, license(checkboxes, populate badge and details in license section), contributing, tests and questions(github username into link and email)
 
