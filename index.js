2c1a9f4b4925c777ba748b57b34bfb5230428c3c
Jpeyton <jpwalk319@gmail.com>
2021-02-16 20:00:13 -0700

    added a README and some pseudocode into index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquire = require('inquirer');
+const fs = require('fs');
+let template = '';
+// needs title, description, table of contents, installation, usage, license(checkboxes, populate badge and details in license section), contributing, tests and questions(github username into link and email)
+
+
+function templateGen() {
+
+}
